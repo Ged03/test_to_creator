0cdfb8185db4f9833bec3f7882cab854b27aca85
ged <nick.cheb2@mail.ru>
2017-07-03 18:00:20 +0500

    Доделал второе задание PHP.

--- PHP/php_task_2.php
+++ PHP/php_task_2.php
@@ -2,14 +2,14 @@
 
 function convertDate($day, $month) 
 {
-   if (checkdate($month, $day, 2017))
+   if (checkdate($month, $day, date('Y'))) //проверка даты на корректность
    {
         $string_months=array('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
-       $month--;
-       return ("$day "."$string_months[$month]");
+        $month--;
+        return ("$day "."$string_months[$month]");
    }
-    else return 'no';
+    else return 'Incorrect date.';
 }
 
-echo convertdate(25, 2);
+echo convertdate(01, 07);
 ?>
