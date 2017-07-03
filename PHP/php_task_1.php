c6a9677b35afba2308b4dd4dba655005e73c9b23
Ged <nick.cheb2@mail.ru>
2017-07-03 22:24:39 +0500

    Доделал третье задание. Добавил проверку входных данных в первом задании.

--- PHP/php_task_1.php
+++ PHP/php_task_1.php
@@ -2,7 +2,9 @@
 
 function deposit($sum, $months, $pct) 
 {
+    if (is_int($sum)&&is_int($months)&&is_int($pct))
     return ($sum+$sum*$pct*$months/1200);
+    else return 'Incorrect input data.';
 }
 
 echo deposit(100, 15, 12);
--- PHP/php_task_3.php
+++ PHP/php_task_3.php
@@ -3,7 +3,7 @@
         <title>Calc</title>
     </head>
     <body>
-        <form method="post" action="#">
+        <form method="post" action="php_task_3.php">
             <input type=text name="num1">
             <input type=radio name="oper" value="1" checked=checked>+
             <input type=radio name="oper" value="2">-
@@ -11,6 +11,31 @@
             <input type=radio name="oper" value="4">/
             <input type=text name="num2">
             <input type="submit">
-        </form><br>
+        </form>
     </body>
 </html>
+
+
+<?php
+//Завершение скрипта, если переменные не установлены
+if (!isset($_POST['num1'])&&!isset($_POST['num2'])) exit;
+
+//Проверка, являются ли введённые значения числами
+if (is_numeric($_POST['num1'])&&is_numeric($_POST['num2']))
+{
+    $num1=$_POST['num1'];
+    $num2=$_POST['num2'];
+    $oper=$_POST['oper'];
+    switch ($oper){
+        case 1: echo 'Результат: '.($num1+$num2);
+        break;
+        case 2: echo 'Результат: '.($num1-$num2);
+        break;
+        case 3: echo 'Результат: '.($num1*$num2);
+        break;
+        case 4: echo 'Результат: '.($num1/$num2);
+        break;
+    }
+}
+else echo 'Введите корректно оба числа.';
+?>
